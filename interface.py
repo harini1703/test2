4dcbf51feca5a516c537e719928d97948ea2a81d
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2024-04-02 22:43:46 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -8,3 +8,6 @@ import pandas as pd
 import numpy as np
 
 st.title(" WIND SPEED PREDICTION")
+st.sidebar.header('Input Parameters')
+feature_input = st.sidebar.number_input("Feature Value", min_value=0.0, max_value=100.0, value=50.0)
+
