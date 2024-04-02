60fafb7bde187d79b47b0ba53d2c3744797e36ea
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2024-04-02 21:57:51 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,9 +1,9 @@
 import pickle
-pickle_in=open("windspeed1.pkl",'rb')
+pickle_in=open('windspeed1.pkl','rb')
 clf=pickle.load(pickle_in)
 
 import streamlit as st
-import numpy as np
 import pandas as pd
+import numpy as np
 
-st.title("Wind speed prediction")
+st.title(" MUSHROOM PREDICTION APPLICATION")
