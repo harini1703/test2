b567fb86f3fb639d992caa65349b00f08f612410
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2024-04-02 22:47:18 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -9,5 +9,5 @@ import numpy as np
 
 st.title(" WIND SPEED PREDICTION")
 st.sidebar.header('Input Parameters')
-MAXIMUM TEMPERATURE = st.sidebar.number_input("Feature Value", min_value=0, max_value=100, value=0)
+maximum  = st.sidebar.number_input("maximum", min_value=0, max_value=100, value=0)
 
