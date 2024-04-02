f83ee342d6c8875ca2e8abc72cc27818fbaea22d
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2024-04-02 22:54:08 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -10,4 +10,9 @@ import numpy as np
 st.title(" WIND SPEED PREDICTION")
 st.sidebar.header('Input Parameters')
 Maximum_Temperature = st.sidebar.number_input("Maximum Temperature", min_value=0, max_value=100, value=0)
+Minimum_Temperature = st.sidebar.number_input("Minimum Temperature", min_value=0, max_value=100, value=0)
+Dry_Bulb_Temperature = st.sidebar.number_input("Dry Bulb Temperature", min_value=0, max_value=100, value=0)
+Wet_Bulb_Temperature = st.sidebar.number_input("Wet Bulb Temperature", min_value=0, max_value=100, value=0)
+Dew_point_Temperature = st.sidebar.number_input("Dew point Temperature", min_value=0, max_value=100, value=0)
+
 
