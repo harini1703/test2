f8ca4abd78ffe61e6eb8d3479ee0fee037b2c169
Hari Lakshmi A.S <93150869+harini1703@users.noreply.github.com>
2024-04-03 22:25:09 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -28,23 +28,19 @@ if nav=="Prediction":
     def predict(Maximum_Temperature,Minimum_Temperature,Dry_Bulb_Temperature,Wet_Bulb_Temperature,Dew_point_Temperature):
         prediction = clf.predict([[Maximum_Temperature,Minimum_Temperature,Dry_Bulb_Temperature,Wet_Bulb_Temperature,Dew_point_Temperature]])
         return int(prediction[0])
-
-def interpret_wind_speed(wind_speed):
-    if wind_speed < 15:
-        return "Wind will blow at low level"
-    elif 50 <= wind_speed <= 60:
-        return "Wind will generate at full capacity"
-    elif wind_speed > 80:
-        return "Turn off wind mill"
-    else:
-        return "Wind speed is in a normal range"
         
-#if st.button('Predict'):
-    #prediction = predict(Maximum_Temperature,Minimum_Temperature,Dry_Bulb_Temperature,Wet_Bulb_Temperature,Dew_point_Temperature)
-    #st.write('Predicted Target Value:', prediction)
-if st.button('Predict'):
-    prediction = predict(Maximum_Temperature, Minimum_Temperature, Dry_Bulb_Temperature, Wet_Bulb_Temperature, Dew_point_Temperature)
-    interpretation = interpret_wind_speed(prediction)
-    st.header('Predicted Wind Speed: ' + str(prediction) + ' km/hr')
-    st.write('Interpretation:',interpretation)
-    #st.title(interpretation)
+        def interpret_wind_speed(wind_speed):
+            if wind_speed < 15:
+                return "Wind will blow at low level"
+            elif 50 <= wind_speed <= 60:
+                return "Wind will generate at full capacity"
+            elif wind_speed > 80:
+                return "Turn off wind mill"
+            else:
+                return "Wind speed is in a normal range"
+                if st.button('Predict'):
+                    prediction = predict(Maximum_Temperature, Minimum_Temperature, Dry_Bulb_Temperature, Wet_Bulb_Temperature, Dew_point_Temperature)
+                    interpretation = interpret_wind_speed(prediction)
+                    st.header('Predicted Wind Speed: ' + str(prediction) + ' km/hr')
+                    st.write('Interpretation:',interpretation)
+        #st.title(interpretation)
